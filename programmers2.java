f184e768b2851fb5d6f49ab3b8e543fa97ee9a8a
MochaShyrup <yeanjun0222@gmail.com>
2020-09-08 19:55:21 +0900

    두정수사이의합

---
+++ programmers2.java
@@ -0,0 +1,24 @@
+package programmers;
+
+public class programmers2 {
+
+	public static void main(String[] args) {
+		int a = 5;
+		int b = 3;
+		int answer = 0;
+		if(a<=b) {
+			for (int i=a; i<=b; i++) {
+			answer += i;
+			}
+		}else {
+			for (int i=b; i<=a; i++) {
+				answer += i;
+			}
+		}
+		System.out.println(answer);
+		
+		
+
+	}
+
+}
