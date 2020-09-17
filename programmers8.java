cb3f83c563b585cc578f5a1c659dd14a08aec043
MochaShyrup <yeanjun0222@gmail.com>
2020-09-17 14:41:15 +0900

    홀수 짝수 구하기

---
+++ programmers8.java
@@ -0,0 +1,16 @@
+package programmers;
+
+public class programmers8 {
+
+	public static void main(String[] args) {
+		int num = 3;
+		String answer ="";
+		if(num%2==0) {
+			answer = "Even";
+		}else {
+			answer ="Odd";
+		}
+		System.out.println(answer);
+		
+	}
+}
