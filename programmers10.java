5acdd6213fc085d51886758d14b5f24a80d3904a
MochaShyrup <yeanjun0222@gmail.com>
2020-09-21 21:41:53 +0900

    두 정수 사이의 합

---
+++ programmers10.java
@@ -0,0 +1,17 @@
+package programmers;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class programmers10 {
+
+	public static void main(String[] args) {
+		class Solution {
+		    public int solution(String s) {
+		        int answer = 0;
+		        answer = Integer.parseInt(s);
+		        return answer;
+		    }
+		}
+	}
+}
